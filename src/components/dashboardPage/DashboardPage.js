1fd8afd9c612e5a4fadc5876f081483094b874ec
Ian Nicholas <ianmnicholas@gmail.com>
2018-09-16 16:10:22 -0400

    First pass at payment history

--- src/components/dashboardPage/DashboardPage.js
+++ src/components/dashboardPage/DashboardPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
+
 import BarChart from './BarChart';
 import Intro from './Intro';
 
 import './dashboardPage.scss';
 
-const DashboardPage = ({ squishedLedger, plan }) => {
+const DashboardPage = ({ squishedLedger, plan, projection, debts }) => {
 	const allDebts = {};
 	if (squishedLedger) {
 		squishedLedger.forEach(entry => {
@@ -19,6 +21,30 @@ const DashboardPage = ({ squishedLedger, plan }) => {
 
 	const showIntro = (!plan.created) && squishedLedger.length === 0;
 
+	const today = moment();
+	const paymentsWindowStart = today.clone().subtract(1, 'month').format('YYYY-MM-DD');
+	const paymentsWindowEnd = today.clone().add(1, 'month').format('YYYY-MM-DD');
+	const payments = Object.entries(projection.ledger).reduce((accumulator, [ymd, entry]) => {
+		if (ymd >= paymentsWindowStart && ymd <= paymentsWindowEnd) {
+			Object.entries(entry).forEach(([debtId, debtEntry]) => {
+				if (debtEntry.payment !== '0.00') {
+					accumulator.push({
+						ymd: ymd,
+						debtId: parseInt(debtId),
+						...debtEntry
+					});
+				}
+			});
+		}
+		return accumulator;
+	}, []);
+	payments.sort((a, b) => {
+		if (a.ymd < b.ymd) {
+			return -1;
+		}
+		return 1;
+	});
+
 	// N.B. BarChart's parent component must have a height defined, else it won't render ಠ_ಠ
 	// N.B. Nivo sucks at making legends. It only looks at the first data item to make the legend...
 	//      And even then, it doesn't consider items that are too small, so you can't just push all
@@ -31,15 +57,36 @@ const DashboardPage = ({ squishedLedger, plan }) => {
 				showIntro
 				? <Intro />
 				: (
-					<div style={{ height: "500px" }}>
+					<div>
 						<h2 className="title is-4">Dashboard</h2>
-						<BarChart
-							data={squishedLedger}
-							keys={Object.keys(allDebts)}
-							width={800}
-							height={400}
-						/>
-					</div >
+						<div className="is-clearfix">
+							<div className="dashboard-chart">
+								<BarChart
+									data={squishedLedger}
+									keys={Object.keys(allDebts)}
+									width={800}
+									height={400}
+								/>
+							</div>
+							{ payments.length > 0 && (
+								<div className="dashboard-payments">
+									<h3 className="title is-5">Payments</h3>
+									<h4 className="subtitle is-6">Recent and upcoming</h4>
+
+									<ol>
+										{ payments.map((payment) => {
+											const debt = Object.values(debts).find((debt) => debt.id === payment.debtId);
+											return (
+												<li key={ payment.ymd + debt.name }>
+													{ payment.ymd }: Pay ${ payment.payment } to { debt.name }
+												</li>
+											);
+										}) }
+									</ol>
+								</div>
+							) }
+						</div>
+					</div>
 				)
 			}
 		</div>
@@ -48,7 +95,9 @@ const DashboardPage = ({ squishedLedger, plan }) => {
 
 const mapStateToProps = state => ({
 	squishedLedger: state.projection.squishedLedger,
-	plan: state.plan
+	plan: state.plan,
+	projection: state.projection,
+	debts: state.debts
 });
 
 export default connect(mapStateToProps)(DashboardPage);
