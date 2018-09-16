0d2e745b27d8defc0b389655953d6ba22966ad86
Ian Nicholas <ianmnicholas@gmail.com>
2018-09-16 14:59:02 -0400

    Wrapping dashboard page in class

--- src/components/dashboardPage/DashboardPage.js
+++ src/components/dashboardPage/DashboardPage.js
@@ -6,19 +6,19 @@ import Intro from './Intro';
 import './dashboardPage.scss';
 
 const DashboardPage = ({ squishedLedger, plan }) => {
-    const allDebts = {};
-    if (squishedLedger) {
-        squishedLedger.forEach(entry => {
-            Object.keys(entry).forEach(debt => {
-                if (debt !== '__time__') {
-                    allDebts[debt] = true;
-                }
-            })
-        })
-    }
+	const allDebts = {};
+	if (squishedLedger) {
+		squishedLedger.forEach(entry => {
+			Object.keys(entry).forEach(debt => {
+				if (debt !== '__time__') {
+					allDebts[debt] = true;
+				}
+			})
+		})
+	}
 
-    const showIntro = (!plan.created) && squishedLedger.length === 0;
-	
+	const showIntro = (!plan.created) && squishedLedger.length === 0;
+
 	// N.B. BarChart's parent component must have a height defined, else it won't render ಠ_ಠ
 	// N.B. Nivo sucks at making legends. It only looks at the first data item to make the legend...
 	//      And even then, it doesn't consider items that are too small, so you can't just push all
@@ -26,25 +26,29 @@ const DashboardPage = ({ squishedLedger, plan }) => {
 	// N.B. This chart isn't really a time series. If you omit a month from the data passed in,
 	//      the chart won't render that month at all
 	return (
-        showIntro
-        ? <Intro />
-        : (
-            <div className="dashboard-page" style={{ height: "500px" }}>
-                <h2 className="title is-4">Dashboard</h2>
-                <BarChart
-                    data={squishedLedger}
-                    keys={Object.keys(allDebts)}
-                    width={800}
-                    height={400}
-                />
-            </div >
-        )
+		<div className="dashboard-page">
+			{
+				showIntro
+				? <Intro />
+				: (
+					<div style={{ height: "500px" }}>
+						<h2 className="title is-4">Dashboard</h2>
+						<BarChart
+							data={squishedLedger}
+							keys={Object.keys(allDebts)}
+							width={800}
+							height={400}
+						/>
+					</div >
+				)
+			}
+		</div>
 	);
 }
 
 const mapStateToProps = state => ({
-    squishedLedger: state.projection.squishedLedger,
-    plan: state.plan
+	squishedLedger: state.projection.squishedLedger,
+	plan: state.plan
 });
 
 export default connect(mapStateToProps)(DashboardPage);
